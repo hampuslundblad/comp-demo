9b4e5e08200959af3ed6b703ec9c6aa64a63f9fd
hampuslundblad <hampus.lundblad@hotmail.se>
2024-11-16 11:10:26 +0100

    stuff

---
+++ src/routes/__root.tsx
@@ -0,0 +1,18 @@
+import { isLocalHost } from "@/utils/env";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+      {isLocalHost() ? <TanStackRouterDevtools /> : <> </>}
+    </>
+  ),
+});
